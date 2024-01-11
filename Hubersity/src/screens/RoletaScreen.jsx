845119df247bbd7fb4f0c35c3b6296904244a0f7
joaorocha75 <40200471@esmad.ipp.pt>
2024-01-11 23:28:31 +0000

    inicio da roleta

--- Hubersity/src/screens/RoletaScreen.jsx
+++ Hubersity/src/screens/RoletaScreen.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import {Text, View,Button} from 'react-native';
-import Toast from 'react-native-toast-message';
+/* import React, { useRef } from 'react';
+import { View, Text, Button } from 'react-native';
+import WheelComponent from 'react-wheel-of-prizes'; // Use default import
 
 const Roleta = () => {
+  const wheelRef = useRef(null);
 
-  const showToast = () => {
-    Toast.show({
-      type: 'success',
-      text1: 'Toast',
-      text2: 'Hello world',
-      autoHide:true,
-      visibilityTime:3000,
-    });
+  const segments = [
+    '30 pontos',
+    'Sem prémio',
+    'Compal',
+    'Sem prémio',
+    'Merenda de Chocolate',
+    'Sem prémio',
+  ];
+
+  const segColors = ['black', '#60BA97', 'black', '#60BA97', 'black', '#60BA97'];
+
+  const onFinished = (winner) => {
+    console.log(winner);
   };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Roleta</Text>
-      <Button title="Show Toast" onPress={showToast} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Spinner wheel Demo for TDC</Text>
+      <WheelComponent
+        segments={segments}
+        segColors={segColors}
+        winningSegment="MM"
+        onFinished={(winner) => onFinished(winner)}
+        primaryColor="black"
+        contrastColor="white"
+        buttonText="Start"
+        isOnlyOnce={false}
+        size={190}
+        upDuration={500}
+        downDuration={600}
+        fontFamily="Helvetica"
+      />
+      <Button
+        title="Start"
+        onPress={() => {
+          wheelRef.current._onPress();
+        }}
+      />
+      <Text>Spin the wheel and win exciting offers</Text>
     </View>
   );
 };
-export default Roleta;
+
+export default Roleta; */
