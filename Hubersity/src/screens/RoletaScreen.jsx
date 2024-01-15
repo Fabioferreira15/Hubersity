f8816e9d0ddf9c8a5d34e7a5b515b53bcc2d927d
joaorocha75 <40200471@esmad.ipp.pt>
2024-01-15 14:53:08 +0000

    roleta

--- Hubersity/src/screens/RoletaScreen.jsx
+++ Hubersity/src/screens/RoletaScreen.jsx
@@ -1,51 +1,120 @@
-/* import React, { useRef } from 'react';
-import { View, Text, Button } from 'react-native';
-import WheelComponent from 'react-wheel-of-prizes'; // Use default import
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+import Voltar from '../assets/icons/Voltar.svg';
+import Header from '../components/Header.jsx';
+import Roleta from '../assets/Roleta.svg';
 
-const Roleta = () => {
-  const wheelRef = useRef(null);
+export default function App({ navigation }) {
+  const [rotationValue] = useState(new Animated.Value(0));
 
-  const segments = [
-    '30 pontos',
-    'Sem prémio',
-    'Compal',
-    'Sem prémio',
-    'Merenda de Chocolate',
-    'Sem prémio',
-  ];
+  const rotateRoleta = () => {
+    // Reset para 0 antes de cada nova rotação
+    rotationValue.setValue(0);
 
-  const segColors = ['black', '#60BA97', 'black', '#60BA97', 'black', '#60BA97'];
-
-  const onFinished = (winner) => {
-    console.log(winner);
+    Animated.timing(rotationValue, {
+      toValue: 1,
+      duration: 1000, // Ajuste a duração conforme necessário
+      useNativeDriver: true,
+    }).start();
   };
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Spinner wheel Demo for TDC</Text>
-      <WheelComponent
-        segments={segments}
-        segColors={segColors}
-        winningSegment="MM"
-        onFinished={(winner) => onFinished(winner)}
-        primaryColor="black"
-        contrastColor="white"
-        buttonText="Start"
-        isOnlyOnce={false}
-        size={190}
-        upDuration={500}
-        downDuration={600}
-        fontFamily="Helvetica"
-      />
-      <Button
-        title="Start"
-        onPress={() => {
-          wheelRef.current._onPress();
-        }}
-      />
-      <Text>Spin the wheel and win exciting offers</Text>
-    </View>
-  );
-};
+  const spin = rotationValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
 
-export default Roleta; */
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Header
+          title="Roleta"
+          iconPosition="left"
+          onPress={() => navigation.navigate('HomeBar')}
+          customIcon={<Voltar />}
+        />
+        <View style={styles.main}>
+          <Text style={styles.txtTitulo}>
+            Aqui tens hipótese de ganhares um brinde todos os dias!
+          </Text>
+          <Text style={styles.txtInfo}>
+            Gira a roleta e testa a tua sorte.
+          </Text>
+        </View>
+        <Animated.View
+          style={[
+            styles.roletaContainer,
+            {
+              transform: [
+                { translateY: -50 },
+                { rotate: spin },
+              ],
+            },
+          ]}
+        >
+          <Roleta style={styles.roleta} />
+        </Animated.View>
+        <TouchableOpacity
+          style={styles.btnGirar}
+          onPress={() => {
+            rotateRoleta();
+            // Adicione aqui qualquer lógica adicional que você queira executar ao girar a roleta
+          }}
+        >
+          <Text style={styles.textBtnGirar}>Spin</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  roletaContainer: {
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingBottom: '15%',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F9FA',
+  },
+  main: {
+    marginTop: '30%',
+    marginLeft: '5%',
+  },
+  txtTitulo: {
+    fontSize: 18,
+    fontFamily: 'BaiJamjuree-Bold',
+    color: '#212529',
+  },
+  txtInfo: {
+    fontSize: 16,
+    fontFamily: 'BaiJamjuree-Regular',
+    color: '#212529',
+  },
+  btnGirar: {
+    alignSelf: 'center',
+    marginTop: -50,
+    marginBottom: '10%',
+    borderRadius: 10,
+    width: '40%',
+    backgroundColor: '#F0D060',
+    verticalAlignText: 'middle',
+    paddingVertical: '2%',
+  },
+  textBtnGirar: {
+    color: '#151C4D',
+    fontSize: 15,
+    fontFamily: 'BaiJamjuree-Bold',
+    textAlign: 'center',
+  },
+});
